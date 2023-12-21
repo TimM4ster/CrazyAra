6280aa5df64ea3c3a647c3c1f0666fe4326c7cb0
TimM4ster <timmichael.krieg@gmail.com>
2023-12-21 16:37:12 +0100

    Add necessary configurations for neural architecture search

--- DeepCrazyhouse/configs/nas_config.py
+++ DeepCrazyhouse/configs/nas_config.py
@@ -6,13 +6,34 @@ Created on 02.12.23
 
 Config definition file used for the neural architecture search. Appends necessary configurations to the main_config.py-file.
 """
+import glob
+
+from nni.nas.experiment import NasExperimentConfig
+
+from DeepCrazyhouse.configs.train_config import TrainConfig
+from DeepCrazyhouse.configs.model_config import ModelConfig
 from DeepCrazyhouse.configs.main_config import main_config
 
-def get_nas_config():
+def fill_nas_config(nas_config: NasExperimentConfig, args):
+    """
+    Fills the nas_config with the necessary configurations. Modify this function to change the configurations for the neural architecture search. Also takes the command-line arguments as input.
+
+    :param nas_config: The nas_config to be filled.
+    """
+    # TODO: Fill nas_config with necessary configurations
+    nas_config.experiment_name = args.experiment_name
+    pass
+
+def get_nas_configs(args, x_val):
     """
     Takes the main_config defined in DeepCrazyhouse/configs/main_config.py and appends the necessary configurations for the neural architecture search.
     """
-    nc = main_config
+    tc = TrainConfig()
+    tc.nb_parts = len(glob.glob(main_config['planes_train_dir'] + '**/*'))
 
-    return nc
+    mc = ModelConfig()
 
+    nc = NasExperimentConfig()
+    fill_nas_config(nc, args)
+
+    return tc, mc, nc
