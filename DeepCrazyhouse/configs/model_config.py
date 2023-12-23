4645c85e4b4c5e827e98977797ee0a73c43f2dcd
TimM4ster <timmichael.krieg@gmail.com>
2023-12-23 11:13:14 +0100

    Remove commata in model_config.py

--- DeepCrazyhouse/configs/model_config.py
+++ DeepCrazyhouse/configs/model_config.py
@@ -16,27 +16,27 @@ class ModelConfig:
     Note not all models support all parameters."""
 
     # all models
-    channels: int = 256,
-    channels_value_head: int = 8,
-    channels_policy_head: int = 81,
-    value_fc_size: int = 256,
+    channels: int = 256
+    channels_value_head: int = 8
+    channels_policy_head: int = 81
+    value_fc_size: int = 256
 
     # resent
-    num_res_blocks: int = 19,
+    num_res_blocks: int = 19
 
     # risev2, risev3, alphavile
-    channels_operating_init: int = 224,
-    channel_expansion: int = 32,
+    channels_operating_init: int = 224
+    channel_expansion: int = 32
 
-    kernels = [3] * 15,
-    se_types = [None] * len(kernels),
-    act_types = ['relu'] * len(kernels),
+    kernels = [3] * 15
+    se_types = [None] * len(kernels)
+    act_types = ['relu'] * len(kernels)
 
     # alphavile
     use_transformers = [False] * len(kernels)
 
-    path_dropout: float = 0.05,
-    kernel_5_channel_ratio: float = 0.5,
+    path_dropout: float = 0.05
+    kernel_5_channel_ratio: float = 0.5
 
     # nextvit
     stage3_repeat: int = 1
